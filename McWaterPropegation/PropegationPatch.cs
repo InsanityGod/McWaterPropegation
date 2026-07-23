fa1e09bd9b5e1473dd5de5b0070206cc9d84a680
InsanityGod <krizziewissie@gmail.com>
2026-07-23 20:06:19 +0200

    Stability improvent (fixes some weird behavior where moving liquid tries to replace source blocks)

--- McWaterPropegation/PropegationPatch.cs
+++ McWaterPropegation/PropegationPatch.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Reflection;
 using System.Reflection.Emit;
 using Vintagestory.API.Common;
+using Vintagestory.API.Config;
+using Vintagestory.API.MathTools;
 using Vintagestory.GameContent;
 
 namespace McWaterPropegation;
@@ -10,8 +12,9 @@ namespace McWaterPropegation;
 [HarmonyPatch]
 public static class PropegationPatch
 {
+    [HarmonyTranspiler]
     [HarmonyPatch(typeof(BlockBehaviorFiniteSpreadingLiquid), "SpreadAndUpdateLiquidLevels")]
-    public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
+    public static IEnumerable<CodeInstruction> SpreadAndUpdateLiquidLevels_Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
     {
         var matcher = new CodeMatcher(instructions, generator);
 
@@ -61,4 +64,33 @@ public static class PropegationPatch
     }
 
     public static int GetRequiredSourceCount(Block block) => block?.BlockMaterial == EnumBlockMaterial.Water ? 2 : 3;
+
+    [HarmonyTranspiler]
+    [HarmonyPatch(typeof(BlockBehaviorFiniteSpreadingLiquid), nameof(BlockBehaviorFiniteSpreadingLiquid.CanSpreadIntoBlock), typeof(Block), typeof(Block), typeof(BlockPos), typeof(BlockPos), typeof(BlockFacing), typeof(IWorldAccessor))]
+    public static IEnumerable<CodeInstruction> CanSpreadIntoBlock_Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
+    {
+        var matcher = new CodeMatcher(instructions, generator);
+
+        var isSourceBlockMethod = AccessTools.Method(typeof(BlockBehaviorFiniteSpreadingLiquid), "IsLiquidSourceBlock");
+
+        matcher.Start()
+            .MatchEndForward(
+                CodeMatch.Calls(isSourceBlockMethod),
+                CodeMatch.WithOpcodes([OpCodes.Brtrue, OpCodes.Brtrue_S])
+            );
+
+        matcher.DefineLabel(out var originalPath);
+        matcher.InsertAfterAndAdvance(
+            CodeInstruction.LoadArgument(0),
+            CodeInstruction.LoadLocal(0),
+            new CodeInstruction(OpCodes.Call, isSourceBlockMethod),
+            new CodeInstruction(OpCodes.Brfalse_S, originalPath),
+            new CodeInstruction(OpCodes.Ldc_I4_0),
+            new CodeInstruction(OpCodes.Ret)
+        );
+        matcher.Advance();
+        matcher.Labels.Add(originalPath);
+
+        return matcher.InstructionEnumeration();
+    }
 }
